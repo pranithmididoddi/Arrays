bdb5d1a59798f8d205161cf90fafb0255ca683ae
Srinivas <pranith13@gmail.com>
2017-02-06 01:54:07 -0600

    remove duplicates

--- src/Solution.java
+++ src/Solution.java
@@ -32,4 +32,15 @@ public class Solution {
         return result;
 
     }
+    public int removeElement(int[] nums, int val) {
+        int i=0;
+
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=val){
+                nums[i]=nums[j];
+                i++;
+            }
+
+        }
+        return i;
 }
