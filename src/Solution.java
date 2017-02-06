4f8ae083d5d04d49c463570b4ebdfbb15ac7706a
Srinivas <pranith13@gmail.com>
2017-02-06 02:09:25 -0600

    remove duplicates

--- src/Solution.java
+++ src/Solution.java
@@ -33,14 +33,29 @@ public class Solution {
 
     }
     public int removeElement(int[] nums, int val) {
-        int i=0;
+        int i = 0;
 
-        for(int j=0;j<nums.length;j++){
-            if(nums[j]!=val){
-                nums[i]=nums[j];
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
                 i++;
             }
 
         }
         return i;
+    }
+        public int findMaxConsecutiveOnes(int[] nums) {
+
+            int count=0, result=0;
+
+            for(int i=0;i<nums.length;i++){
+                if(nums[i]==1){
+                    count++;
+                    result=Math.max(count,result);
+                }else{
+                    count=0;
+                }
+            }
+            return result;
+        }
 }
