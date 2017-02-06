ba3c81374eb4ead08da602bb50a6568ad74bcf1e
Srinivas <pranith13@gmail.com>
2017-02-06 12:53:43 -0600

    remove duplicates

--- src/Solution.java
+++ src/Solution.java
@@ -58,4 +58,41 @@ public class Solution {
             }
             return result;
         }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result=new ArrayList<>();
+
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-2;i++){
+            if(i==0 || nums[i]>nums[i-1]){
+                int j=i+1;
+                int k=nums.length-1;
+
+                while(j<k){
+                    if(nums[i]+nums[j]+nums[k]==0){
+                        List<Integer> list=new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[k]);
+                        result.add(list);
+                        j++;
+                        k--;
+
+                        while(j<k && nums[j]==nums[j-1]){
+                            j++;
+                        }
+                        while(j<k && nums[k]==nums[k+1]){
+                            k--;
+                        }
+                    }else if(nums[i]+nums[j]+nums[k]<0){
+                        j++;
+                    }
+                    else{
+                        k--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
 }
