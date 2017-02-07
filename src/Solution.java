a3fedd36699e14674a44a7babb5819f4ba4ca790
Srinivas <pranith13@gmail.com>
2017-02-06 18:02:55 -0600

    remove duplicates

--- src/Solution.java
+++ src/Solution.java
@@ -95,4 +95,36 @@ public class Solution {
         }
         return result;
     }
+
+    public int threeSumClosest(int[] nums, int target) {
+        int min=Integer.MAX_VALUE;
+        int result=0;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            int j=i+1;
+            int k=nums.length-1;
+
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                int diff=Math.abs(target-sum);
+
+                if(diff==0){
+                    return sum;
+                }
+
+                if(diff<min){
+                    min=diff;
+                    result=sum;
+                }
+                if(sum<=target){
+                    j++;
+                }else{
+                    k--;
+                }
+
+            }
+        }
+        return result;
+    }
+
 }
