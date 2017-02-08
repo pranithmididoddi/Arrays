d27f4713d31ce05e1078c1f36970f427748d59fb
Srinivas <pranith13@gmail.com>
2017-02-07 18:05:42 -0600

    remove duplicates

--- src/Solution.java
+++ src/Solution.java
@@ -156,4 +156,47 @@ public class Solution {
         }
         return list;
     }
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+
+        List<List<Integer>> list=new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-3;i++){
+            if(i!=0 && nums[i]==nums[i-1] ){
+                continue;
+            }
+            for(int j=i+1;j<nums.length-2;j++){
+                if(j!=i+1 && nums[j]==nums[j-1]){
+                    continue;
+                }
+                int k=j+1;
+                int l=nums.length-1;
+                while(k<l){
+                    if(nums[i]+nums[j]+nums[k]+nums[l] < target){
+                        k++;
+                    }else if(nums[i]+nums[j]+nums[k]+nums[l] > target){
+                        l--;
+                    }else{
+                        List<Integer> singlelist=new ArrayList<>();
+                        singlelist.add(nums[i]);
+                        singlelist.add(nums[j]);
+                        singlelist.add(nums[k]);
+                        singlelist.add(nums[l]);
+                        list.add(singlelist);
+
+                        k++;
+                        l--;
+
+                        while(k<l && nums[l]==nums[l+1]){
+                            l--;
+                        }
+                        while(k<l && nums[k]==nums[k-1]){
+                            k++;
+                        }
+                    }
+                }
+            }
+        }
+        return list;
+    }
 }
