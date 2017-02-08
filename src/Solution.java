c4c8022d34fb96bd161ae69981bd0750edc56d83
Srinivas <pranith13@gmail.com>
2017-02-08 17:37:24 -0600

    remove duplicates

--- src/Solution.java
+++ src/Solution.java
@@ -277,4 +277,23 @@ public class Solution {
         }
         return false;
     }
-}
+
+    public int[] twoSumii(int[] nums, int target) {
+        Map<Integer, Integer> map=new HashMap<>();
+
+        int[] result=new int[2];
+
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                int res=map.get(nums[i]);
+                result[0]=res+1;
+                result[1]=i+1;
+                break;
+            }else{
+                map.put(target-nums[i],i);
+            }
+
+        }
+        return result;
+
+    }
