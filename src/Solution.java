a2dbe9c46f4f0e45f3aa91074ae59c31ec84cbb7
Srinivas <pranith13@gmail.com>
2017-02-08 21:41:33 -0600

    remove duplicates

--- src/Solution.java
+++ src/Solution.java
@@ -319,4 +319,22 @@ public class Solution {
         }
         return num;
     }
+
+    public List<String> summaryRanges(int[] nums) {
+        List<String> list=new ArrayList<>();
+
+        for(int i=0;i<nums.length;i++){
+            int a=nums[i];
+            while(i+1<nums.length && nums[i+1]-nums[i]==1){
+                i++;
+            }
+            if(a!=nums[i]){
+                list.add(a+"->"+nums[i]);
+            }
+            else{
+                list.add(nums[i]+"");
+            }
+        }
+        return list;
+    }
     }
