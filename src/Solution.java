778669947c8ad80bbfca3991a10aaa06204fb069
Srinivas <pranith13@gmail.com>
2017-02-09 00:39:46 -0600

    remove duplicates

--- src/Solution.java
+++ src/Solution.java
@@ -358,4 +358,16 @@ public class Solution {
         }
         return num;
     }
+    public int maxProfit(int[] prices) {
+        int minPrice=Integer.MAX_VALUE;
+        int maxProfit=0;
+
+        for(int i=0;i<prices.length;i++){
+            if(prices[i]<minPrice){
+                minPrice=prices[i];
+            }
+            maxProfit=Math.max(maxProfit,prices[i]-minPrice);
+        }
+        return maxProfit;
+    }
     }
