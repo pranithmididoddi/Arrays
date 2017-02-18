55482c0a1564a7fcc332516c0b40f953da420d84
Srinivas <pranith13@gmail.com>
2017-02-18 15:30:03 -0600

    remove duplicates

--- src/Solution.java
+++ src/Solution.java
@@ -434,4 +434,23 @@ public class Solution {
         }
         return array[m-1][n-1];
     }
+
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+
+        List<Integer> list=new ArrayList<>();
+
+        for(int i=0;i<nums.length;i++){
+            int val=Math.abs(nums[i])-1;
+            if(nums[val]>0){
+                nums[val]=-nums[val];
+            }
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                list.add(i+1);
+            }
+        }
+        return list;
+    }
     }
