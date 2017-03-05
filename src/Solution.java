b2d4190db0169823b347e3abda1e8d1012c75bc3
Srinivas <pranith13@gmail.com>
2017-03-05 13:15:23 -0600

    finding the missing number

--- src/Solution.java
+++ src/Solution.java
@@ -730,4 +730,20 @@ public class Solution {
 
         return false;
     }
+/**Find the missing number*/
+    public int missingNumber(int[] nums) {
+
+        int sum=0;
+
+        for(int i=0;i<nums.length;i++){
+            sum=sum+nums[i];
+        }
+
+        int n=nums.length;
+
+        int result=n*(n+1)/2 - sum;
+
+        return result;
+
+    }
     }
