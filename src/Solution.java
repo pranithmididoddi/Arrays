b4c66e80b7fc4afbe3e64d92bee5783cc1c9634b
Srinivas <pranith13@gmail.com>
2017-03-10 11:18:34 -0600

    search for a position in matrix

--- src/Solution.java
+++ src/Solution.java
@@ -746,4 +746,31 @@ public class Solution {
         return result;
 
     }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        if(matrix==null || matrix.length==0 || matrix[0].length==0) return false;
+
+        int m=matrix.length;
+        int n=matrix[0].length;
+
+        int start=0;
+        int end=m*n-1;
+
+        while(start<=end){
+            int mid=(start+end)/2;
+
+            int x=mid/n;
+            int y=mid%n;
+
+            if(matrix[x][y]==target){
+                return true;
+            }else if(matrix[x][y]<target){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return false;
+    }
     }
